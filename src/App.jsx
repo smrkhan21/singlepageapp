57844b04a63dc5cca7c288b4debf35bc680e8b00
Sameer Khan <sameerpathan1997@gmail.com>
2023-11-10 17:43:30 +0530

    Updated project

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import About from './components/About.jsx';
 import Home from "./components/Home.jsx";
 import MyFooter from './components/MyFooter.jsx';
-import NavBar from "./components/NavBar.jsx";
+import NavBar from "./components/Navbar.jsx";
 import Newsletter from './components/Newsletter.jsx';
 import Products from './components/Products.jsx';
 import Services from './components/Services.jsx';
