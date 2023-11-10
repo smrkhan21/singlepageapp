d5275d6009500c2cb978d56c76476b955fb97d61
Sameer Khan <sameerpathan1997@gmail.com>
2023-11-10 17:50:36 +0530

    Updated images path

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import icon1 from './../assets/icon_1.png';
+import icon2 from './../assets/icon_2.png';
+import icon3 from './../assets/icon_3.png';
+import logo1 from './../assets/logo_1.png';
+import logo2 from './../assets/logo_2.png';
+import logo3 from './../assets/logo_3.png';
+import logo4 from './../assets/logo_4.png';
+import logo5 from './../assets/logo_5.png';
+import logo6 from './../assets/logo_6.png';
+import logo7 from './../assets/logo_7.png';
 
 const Services = () => {
     const services = [
-        {id: 1, image:'/src/assets/icon_1.png', title:'Membership Organisations', description: 'Our membership management software provides full automation of membership renewals and payments'},
-        {id: 2, image:'/src/assets/icon_2.png', title:'National Associations', description: 'Our membership management software provides full automation of membership renewals and payments'},
-        {id: 3, image:'/src/assets/icon_3.png', title:'Clubs And Groups', description: 'Our membership management software provides full automation of membership renewals and payments'},
+        {id: 1, image:icon1, title:'Membership Organisations', description: 'Our membership management software provides full automation of membership renewals and payments'},
+        {id: 2, image:icon2, title:'National Associations', description: 'Our membership management software provides full automation of membership renewals and payments'},
+        {id: 3, image:icon3, title:'Clubs And Groups', description: 'Our membership management software provides full automation of membership renewals and payments'},
     ]
   return (
     <div id="services" className='md:px-14 px-4 py-16 max-w-screen-2xl mx-auto'>
@@ -12,13 +22,13 @@ const Services = () => {
             <h2 className='text-4xl text-neutralDGrey font-semibold mb-2'>Our Clients</h2>
             <p className='text-neutralGrey'>We have been working with some Fortune 500+ clients</p>
             <div className='my-12 flex flex-wrap justify-between items-center gap-8'>
-                <img src="/src/assets/logo_1.png" alt="logo_1" />
-                <img src="/src/assets/logo_2.png" alt="logo_2" />
-                <img src="/src/assets/logo_3.png" alt="logo_3" />
-                <img src="/src/assets/logo_4.png" alt="logo_4" />
-                <img src="/src/assets/logo_5.png" alt="logo_5" />
-                <img src="/src/assets/logo_6.png" alt="logo_6" />
-                <img src="/src/assets/logo_7.png" alt="logo_7" />
+                <img src={logo1} alt="logo_1" />
+                <img src={logo2} alt="logo_2" />
+                <img src={logo3} alt="logo_3" />
+                <img src={logo4} alt="logo_4" />
+                <img src={logo5} alt="logo_5" />
+                <img src={logo6} alt="logo_6" />
+                <img src={logo7} alt="logo_7" />
             </div>
         </div>
         <div className="mt-20 md:w-1/2 mx-auto text-center">
